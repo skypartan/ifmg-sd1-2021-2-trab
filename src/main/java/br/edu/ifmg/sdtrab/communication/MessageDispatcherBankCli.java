19b25a7a9ec317e35b65d9dc7a1e831ef5a96ea8
Weverton <wevertonrodriguesarantes@gmail.com>
2021-07-12 17:41:28 -0300

    JGroups - MessageDispatcherBankCli

---
+++ src/main/java/br/edu/ifmg/sdtrab/communication/MessageDispatcherBankCli.java
@@ -0,0 +1,54 @@
+package br.edu.ifmg.sdtrab.communication;
+
+import org.jgroups.*;
+import org.jgroups.blocks.*;
+import org.jgroups.blocks.cs.ReceiverAdapter;
+import org.jgroups.protocols.pbcast.ViewHandler;
+import org.jgroups.util.RspList;
+import org.jgroups.util.Util;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+
+class MessageDispatcherBankCli implements RequestHandler {
+    JChannel          channel;
+    MessageDispatcher disp;
+    RspList           rsp_list;
+    String            props; // to be set by application programmer
+
+    public void start() throws Exception {
+        channel=new JChannel(props);
+        disp=new MessageDispatcher(channel, this);
+        channel.connect("MessageDispatcherTestGroup");
+
+        for(int i=0; i < 10; i++) {
+            Util.sleep(100);
+            System.out.println("Casting message #" + i);
+            byte[] pl=("Number #" + i).getBytes();
+            RequestOptions opcoes = new RequestOptions();
+            opcoes.setMode(ResponseMode.GET_ALL); // ESPERA receber a resposta da MAIORIA dos membros (MAJORITY) // Outras opções: ALL, FIRST, NONE
+            opcoes.setAnycasting(false);
+            opcoes.SYNC();
+            rsp_list=disp.castMessage(null,
+                    new BytesMessage(null, pl, 0, pl.length),
+                    opcoes);
+            System.out.println("Responses:\n" +rsp_list);
+        }
+        Util.close(disp,channel);
+    }
+
+    public Object handle(Message msg) throws Exception {
+        System.out.println("handle(): " + msg);
+        return "Success!";
+    }
+
+    public static void main(String[] args) {
+        try {
+            new MessageDispatcherBankCli().start();
+        }
+        catch(Exception e) {
+            System.err.println(e);
+        }
+    }
+}
