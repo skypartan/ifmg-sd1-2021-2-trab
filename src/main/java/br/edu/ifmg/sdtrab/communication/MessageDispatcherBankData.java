de600dacc86420921c4422d99319f32a8a663ffb
Weverton <wevertonrodriguesarantes@gmail.com>
2021-07-21 10:22:58 -0300

    Final - Handle

--- src/main/java/br/edu/ifmg/sdtrab/communication/MessageDispatcherBankData.java
+++ src/main/java/br/edu/ifmg/sdtrab/communication/MessageDispatcherBankData.java
@@ -8,54 +8,56 @@ import org.jgroups.util.Util;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Scanner;
+
+import static java.lang.Boolean.FALSE;
 
 
 class MessageDispatcherBankData implements RequestHandler {
-    JChannel          channel;
+    JChannel channel;
     MessageDispatcher disp;
-    RspList           rsp_list;
-    String            props; // to be set by application programmer
+    RspList rsp_list;
+    String props; // to be set by application programmer
     //LockService lock_service;
 
-    public void start() throws Exception {
+    public void start(boolean isMensage, HashMap<String, String> h, RequestOptions opcoes) throws Exception {
         //ch=new JChannel("/home/bela/locking.xml");
-        channel=new JChannel(props);
+        channel = new JChannel(props);
         //LockService lock_service=new LockService(channel);
-        disp=new MessageDispatcher(channel, this);
+        disp = new MessageDispatcher(channel, this);
         channel.connect("MessageDispatcherTestGroup");
         //Lock lock = lock_service.getLock("mylock"); // gets a cluster-wide lock
         //lock.lock();
         //try {
-            for (int i = 0; i < 10; i++) {
-                Util.sleep(100);
-                HashMap<String, String> test = new HashMap();
-                test.put("tipo", "NEW");
-                test.put( "usuario", "user");
-                test.put("senha", "123");
-                RequestOptions opcoes = new RequestOptions();
-                opcoes.setMode(ResponseMode.GET_ALL);
-                opcoes.setAnycasting(false);
-                System.out.println("Casting message #" + i);
-                opcoes.ASYNC();
-                ObjectMessage msg = new ObjectMessage(null).setObject(test);
-                rsp_list = disp.castMessage(null,
-                        msg,
-                        opcoes);
-                System.out.println("Responses:\n" + rsp_list);
-            }
-            Util.close(disp, channel);
+        if (isMensage) {
+
+            mensagem(h, opcoes);
+        } else {
+            eventLoop();
+        }
+        Util.close(disp, channel);
         //} finally {
-         //   lock.unlock();
+        //   lock.unlock();
         //}
+    }
+
+    public String mensagem(HashMap<String, String> h, RequestOptions opcoes) throws Exception {
+
+        ObjectMessage msg = new ObjectMessage(null).setObject(h);
+        rsp_list = disp.castMessage(null,
+                msg,
+                opcoes);
+        System.out.println("Responses:\n" + rsp_list);
+        return "";
     }
 
     @Override
     public Object handle(org.jgroups.Message msg) throws Exception {
         HashMap msgF = msg.getObject();
-        if (msgF.get("tipo").equals("NEW")){
+        if (msgF.get("tipo").equals("NEW")) {
             System.out.println(msgF.get("tipo"));
-            System.out.println(msgF.get("usuario")+" "+msgF.get("senha"));
-        } else if (msgF.get("tipo").equals("LOGIN")){
+            System.out.println(msgF.get("usuario") + " " + msgF.get("senha"));
+        } else if (msgF.get("tipo").equals("LOGIN")) {
 
         } else if (msgF.get("tipo").equals("TRANSFER")) {
 
@@ -64,16 +66,35 @@ class MessageDispatcherBankData implements RequestHandler {
         } else if (msgF.get("tipo").equals("BALACE")) {
 
         }
-        System.out.println("handle(): " +msg.getObject());
+        System.out.println("handle(): " + msg.getObject());
 
         return "Success!";
     }
 
+    private void eventLoop() throws Exception {
+        while (channel.getView().getMembers().toArray().length != 0)
+            Util.sleep(100); // aguarda o primeiro membro sair do cluster
+
+
+    }//eventLoop
+
     public static void main(String[] args) {
         try {
-            new MessageDispatcherBankData().start();
-        }
-        catch(Exception e) {
+
+            for (int i = 0; i < 10; i++) {
+                Util.sleep(100);
+                HashMap<String, String> test = new HashMap();
+                test.put("tipo", "NEW");
+                test.put("usuario", "user");
+                test.put("senha", "123");
+                RequestOptions opcoes = new RequestOptions();
+                opcoes.setMode(ResponseMode.GET_ALL);
+                opcoes.setAnycasting(false);
+                System.out.println("Casting message #" + i);
+                opcoes.SYNC();
+                new MessageDispatcherBankData().start(true, test, opcoes);
+            }
+        } catch (Exception e) {
             System.err.println(e);
         }
     }
