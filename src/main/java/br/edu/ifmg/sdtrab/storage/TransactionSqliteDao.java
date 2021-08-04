802a36eacab2e9a681a54f2766bff21244603981
Weverton <wevertonrodriguesarantes@gmail.com>
2021-08-04 10:42:19 -0300

    SQLite - No try

--- src/main/java/br/edu/ifmg/sdtrab/storage/TransactionSqliteDao.java
+++ src/main/java/br/edu/ifmg/sdtrab/storage/TransactionSqliteDao.java
@@ -3,6 +3,7 @@ package br.edu.ifmg.sdtrab.storage;
 import br.edu.ifmg.sdtrab.entity.Transaction;
 
 import java.sql.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,184 +17,156 @@ public class TransactionSqliteDao {
         userSqliteDao = new UserSqliteDao();
     }
 
-    public void save(Transaction transaction, String dbName) {
+    public void save(Transaction transaction, String dbName) throws SQLException {
         String sql = "INSERT INTO transactionBank (sender_id, receiver_id, ammount, time) VALUES(?,?,?,?)";
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, transaction.getSender().getId());
-            pstmt.setInt(2, transaction.getReceiver().getId());
-            pstmt.setBigDecimal(3, transaction.getValue());
-            pstmt.setString(4, transaction.getTime().toString());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, transaction.getSender().getId());
+        pstmt.setInt(2, transaction.getReceiver().getId());
+        pstmt.setBigDecimal(3, transaction.getValue());
+        pstmt.setString(4, transaction.getTime().toString());
+        pstmt.executeUpdate();
+
     }
 
-    public ArrayList<Transaction> selectAll(String dbName) {
+    public ArrayList<Transaction> selectAll(String dbName) throws SQLException, ParseException {
         String sql = "SELECT * FROM transactionBank";
         ArrayList<Transaction> list = new ArrayList<>();
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+        Connection conn = connection;
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
 
-            // loop through the result set
-            while (rs.next()) {
-                Transaction transaction = new Transaction();
-                transaction.setId(rs.getInt("id"));
-                transaction.setValue(rs.getBigDecimal("ammount"));
-                transaction.setSender(userSqliteDao.find(rs.getInt("sender_id"), dbName));
-                transaction.setReceiver(userSqliteDao.find(rs.getInt("receiver_id"), dbName));
-                try {
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
-                    Date parsedDate = (Date) dateFormat.parse(rs.getString("time"));
-                    Timestamp timestamp = new Timestamp(parsedDate.getTime());
-                    transaction.setTime(timestamp);
-                } catch(Exception e) {
-                    System.out.println("Error: " + e);
-                }
-                list.add(transaction);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        // loop through the result set
+        while (rs.next()) {
+            Transaction transaction = new Transaction();
+            transaction.setId(rs.getInt("id"));
+            transaction.setValue(rs.getBigDecimal("ammount"));
+            transaction.setSender(userSqliteDao.find(rs.getInt("sender_id"), dbName));
+            transaction.setReceiver(userSqliteDao.find(rs.getInt("receiver_id"), dbName));
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+            Date parsedDate = (Date) dateFormat.parse(rs.getString("time"));
+            Timestamp timestamp = new Timestamp(parsedDate.getTime());
+            transaction.setTime(timestamp);
+
+            list.add(transaction);
         }
+
         return list;
     }
 
-    public void update(Transaction transaction, String dbName) {
+    public void update(Transaction transaction, String dbName) throws SQLException {
         String sql = "UPDATE transactionBank SET sender_id = ?,"
                 + " receiver_id = ?,"
                 + " ammount = ?,"
                 + " time = ?"
                 + "WHERE id = ?";
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            // set the corresponding param
-            pstmt.setInt(1, transaction.getSender().getId());
-            pstmt.setInt(2, transaction.getReceiver().getId());
-            pstmt.setBigDecimal(3, transaction.getValue());
-            pstmt.setString(4, transaction.getTime().toString());
-            pstmt.setInt(5, transaction.getId());
+        // set the corresponding param
+        pstmt.setInt(1, transaction.getSender().getId());
+        pstmt.setInt(2, transaction.getReceiver().getId());
+        pstmt.setBigDecimal(3, transaction.getValue());
+        pstmt.setString(4, transaction.getTime().toString());
+        pstmt.setInt(5, transaction.getId());
 
-            // update
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        // update
+        pstmt.executeUpdate();
+
     }
 
-    public void delete(Transaction transaction,String dbName) {
+    public void delete(Transaction transaction, String dbName) throws SQLException {
         String sql = "DELETE FROM transactionBank WHERE id = ?";
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            // set the corresponding param
-            pstmt.setInt(1, transaction.getId());
-            // execute the delete statement
-            pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        // set the corresponding param
+        pstmt.setInt(1, transaction.getId());
+        // execute the delete statement
+        pstmt.executeUpdate();
     }
 
-    public Transaction find(int id, String dbName) {
+    public Transaction find(int id, String dbName) throws SQLException, ParseException {
         String sql = "SELECT * FROM transactionBank WHERE id = ?";
         Transaction transaction = new Transaction();
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
 
-            ResultSet rs  = pstmt.executeQuery();
+        ResultSet rs = pstmt.executeQuery();
 
-            // loop through the result set
+        // loop through the result set
+        transaction.setId(rs.getInt("id"));
+        transaction.setValue(rs.getBigDecimal("ammount"));
+        transaction.setSender(userSqliteDao.find(rs.getInt("sender_id"), dbName));
+        transaction.setReceiver(userSqliteDao.find(rs.getInt("receiver_id"), dbName));
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+        Date parsedDate = (Date) dateFormat.parse(rs.getString("time"));
+        Timestamp timestamp = new Timestamp(parsedDate.getTime());
+        transaction.setTime(timestamp);
+
+        return transaction;
+    }
+
+    public ArrayList<Transaction> findbyReceiverId(int id, String dbName) throws SQLException, ParseException {
+        String sql = "SELECT * FROM transactionBank WHERE receiver_id = ?";
+        ArrayList<Transaction> list = new ArrayList<>();
+        connection = new Connector().connect(dbName);
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
+
+        ResultSet rs = pstmt.executeQuery();
+
+        // loop through the result set
+        while (rs.next()) {
+            Transaction transaction = new Transaction();
             transaction.setId(rs.getInt("id"));
             transaction.setValue(rs.getBigDecimal("ammount"));
             transaction.setSender(userSqliteDao.find(rs.getInt("sender_id"), dbName));
             transaction.setReceiver(userSqliteDao.find(rs.getInt("receiver_id"), dbName));
-            try {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
-                Date parsedDate = (Date) dateFormat.parse(rs.getString("time"));
-                Timestamp timestamp = new Timestamp(parsedDate.getTime());
-                transaction.setTime(timestamp);
-            } catch(Exception e) {
-                System.out.println("Error: " + e);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return transaction;
-    }
 
-    public ArrayList<Transaction> findbyReceiverId(int id,String dbName) {
-        String sql = "SELECT * FROM transactionBank WHERE receiver_id = ?";
-        ArrayList<Transaction> list = new ArrayList<>();
-        connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS", Locale.ENGLISH);
+            Date date = formatter.parse(rs.getString("time"));
+            Timestamp timestamp = new Timestamp(date.getTime());
+            transaction.setTime(timestamp);
 
-            ResultSet rs  = pstmt.executeQuery();
+            list.add(transaction);
 
-            // loop through the result set
-            while (rs.next()) {
-                Transaction transaction = new Transaction();
-                transaction.setId(rs.getInt("id"));
-                transaction.setValue(rs.getBigDecimal("ammount"));
-                transaction.setSender(userSqliteDao.find(rs.getInt("sender_id"), dbName));
-                transaction.setReceiver(userSqliteDao.find(rs.getInt("receiver_id"), dbName));
-                try {
-                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS", Locale.ENGLISH);
-                    Date date = formatter.parse(rs.getString("time"));
-                    Timestamp timestamp = new Timestamp(date.getTime());
-                    transaction.setTime(timestamp);
-                }catch (Exception e){
-                    System.out.println("Erro: "+ e);
-                }
-
-                list.add(transaction);
-           }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
         return list;
     }
 
-    public ArrayList<Transaction> findbySenderId(int id,String dbName) {
+    public ArrayList<Transaction> findbySenderId(int id, String dbName) throws SQLException, ParseException {
         String sql = "SELECT * FROM transactionBank WHERE sender_id = ?";
         ArrayList<Transaction> list = new ArrayList<>();
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
 
-            ResultSet rs  = pstmt.executeQuery();
+        ResultSet rs = pstmt.executeQuery();
 
-            // loop through the result set
-            while (rs.next()) {
-                Transaction transaction = new Transaction();
-                transaction.setId(rs.getInt("id"));
-                transaction.setValue(rs.getBigDecimal("ammount"));
-                transaction.setSender(userSqliteDao.find(rs.getInt("sender_id"), dbName));
-                transaction.setReceiver(userSqliteDao.find(rs.getInt("receiver_id"), dbName));
-                try {
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
-                    Date parsedDate = (Date) dateFormat.parse(rs.getString("time"));
-                    Timestamp timestamp = new Timestamp(parsedDate.getTime());
-                    transaction.setTime(timestamp);
-                } catch(Exception e) {
-                    System.out.println("Error: " + e);
-                }
-                list.add(transaction);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        // loop through the result set
+        while (rs.next()) {
+            Transaction transaction = new Transaction();
+            transaction.setId(rs.getInt("id"));
+            transaction.setValue(rs.getBigDecimal("ammount"));
+            transaction.setSender(userSqliteDao.find(rs.getInt("sender_id"), dbName));
+            transaction.setReceiver(userSqliteDao.find(rs.getInt("receiver_id"), dbName));
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+            Date parsedDate = (Date) dateFormat.parse(rs.getString("time"));
+            Timestamp timestamp = new Timestamp(parsedDate.getTime());
+            transaction.setTime(timestamp);
+
+            list.add(transaction);
         }
         return list;
     }
--- src/main/java/br/edu/ifmg/sdtrab/storage/UserSqliteDao.java
+++ src/main/java/br/edu/ifmg/sdtrab/storage/UserSqliteDao.java
@@ -9,134 +9,122 @@ import java.util.ArrayList;
 public class UserSqliteDao {
     private Connection connection;
 
-    public void insert(User user,String dbName) {
+    public void insert(User user, String dbName) throws SQLException {
         String sql = "INSERT INTO userBank (name, password, balance) VALUES(?,?,?)";
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, user.getName());
-            pstmt.setString(2, user.getPasswordHash());
-            pstmt.setBigDecimal(3, user.getBalance());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        pstmt.setString(1, user.getName());
+        pstmt.setString(2, user.getPasswordHash());
+        pstmt.setBigDecimal(3, user.getBalance());
+        pstmt.executeUpdate();
+
     }
 
-    public ArrayList<User> selectAll(String dbName) {
+    public ArrayList<User> selectAll(String dbName) throws SQLException {
         String sql = "SELECT * FROM userBank";
         var list = new ArrayList<User>();
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
 
-            // loop through the result set
-            while (rs.next()) {
-                User user = new User();
-                user.setId(rs.getInt("id"));
-                user.setName(rs.getString("name"));
-                user.setPasswordHash(rs.getString("password"));
-                user.setBalance(rs.getBigDecimal("balance"));
-                list.add(user);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        Connection conn = connection;
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+
+
+        // loop through the result set
+        while (rs.next()) {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setName(rs.getString("name"));
+            user.setPasswordHash(rs.getString("password"));
+            user.setBalance(rs.getBigDecimal("balance"));
+            list.add(user);
         }
+
         return list;
     }
 
-    public BigDecimal sumMoney(String dbName) {
+    public BigDecimal sumMoney(String dbName) throws SQLException {
         String sql = "SELECT SUM(balance) FROM userBank";
+
         BigDecimal bg = null;
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
 
-            // loop through the result set
-            bg = rs.getBigDecimal("SUM(balance)");
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        Connection conn = connection;
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        // loop through the result set
+        bg = rs.getBigDecimal("SUM(balance)");
         return bg;
     }
 
-    public void update(User user, String dbName) {
+    public void update(User user, String dbName) throws SQLException {
+
         String sql = "UPDATE userBank SET name = ?, "
                 + "password = ?, "
                 + "balance = ?"
                 + "WHERE id = ?";
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            // set the corresponding param
-            pstmt.setString(1, user.getName());
-            pstmt.setString(2, user.getPasswordHash());
-            pstmt.setBigDecimal(3, user.getBalance());
-            pstmt.setInt(4, user.getId());
+        // set the corresponding param
+        pstmt.setString(1, user.getName());
+        pstmt.setString(2, user.getPasswordHash());
+        pstmt.setBigDecimal(3, user.getBalance());
+        pstmt.setInt(4, user.getId());
 
-            // update
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        // update
+        pstmt.executeUpdate();
+
     }
 
-    public void delete(int id, String dbName) {
+    public void delete(int id, String dbName) throws SQLException {
         String sql = "DELETE FROM userBank WHERE id = ?";
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            // set the corresponding param
-            pstmt.setInt(1, id);
-            // execute the delete statement
-            pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        // set the corresponding param
+        pstmt.setInt(1, id);
+        // execute the delete statement
+        pstmt.executeUpdate();
     }
 
-    public User find(int id, String dbName) {
+    public User find(int id, String dbName) throws SQLException {
         String sql = "SELECT * FROM userBank WHERE id = ?";
         User user = new User();
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
 
-            ResultSet rs  = pstmt.executeQuery();
-            // loop through the result set
-            user.setId(rs.getInt("id"));
-            user.setName(rs.getString("name"));
-            user.setPasswordHash(rs.getString("password"));
-            user.setBalance(rs.getBigDecimal("balance"));
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        ResultSet rs = pstmt.executeQuery();
+        // loop through the result set
+        user.setId(rs.getInt("id"));
+        user.setName(rs.getString("name"));
+        user.setPasswordHash(rs.getString("password"));
+        user.setBalance(rs.getBigDecimal("balance"));
+
         return user;
     }
 
-    public User find(String name, String dbName) {
+    public User find(String name, String dbName) throws SQLException {
         String sql = "SELECT * FROM userBank WHERE name = ?";
         User user = new User();
         connection = new Connector().connect(dbName);
-        try (Connection conn = connection;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, name);
-            ResultSet rs  = pstmt.executeQuery();
-            // loop through the result set
-            user.setId(rs.getInt("id"));
-            user.setName(rs.getString("name"));
-            user.setPasswordHash(rs.getString("password"));
-            user.setBalance(rs.getBigDecimal("balance"));
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        Connection conn = connection;
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, name);
+        ResultSet rs = pstmt.executeQuery();
+        // loop through the result set
+        user.setId(rs.getInt("id"));
+        user.setName(rs.getString("name"));
+        user.setPasswordHash(rs.getString("password"));
+        user.setBalance(rs.getBigDecimal("balance"));
+
         return user;
     }
 
